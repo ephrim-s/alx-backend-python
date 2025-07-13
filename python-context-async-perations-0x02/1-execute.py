caca9fadea1d9e1e8dd131db8b7d22d342786921
ephrim-s <ephrims5@gmial.com>
2025-07-13 23:51:17 +0000

    updated

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+#  Custom context manager for handling connection and query execution
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+        self.conn = None
+        self.results = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        cursor = self.conn.cursor()
+        cursor.execute(self.query, self.params)
+        self.results = cursor.fetchall()
+        return self.results
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            self.conn.close()
+
+
+#  Usage
+query = "SELECT * FROM users WHERE age > ?"
+params = (25,)
+
+with ExecuteQuery('users.db', query, params) as results:
+    print(results)
